9d71ae83de4a43529ad219cfb3620990540f1c07
Stef Boerjan <stef.boerjan@gmail.com>
2023-09-26 11:30:38 +0200

    [fix] added reducer to store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import countriesReducer from './slices/countries/countriesSlice'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    countries: countriesReducer,
+  },
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
