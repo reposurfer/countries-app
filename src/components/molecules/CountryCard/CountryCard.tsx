5a25f511e06a85a03dd67b57c1e9cd9951360212
Stef Boerjan <stef.boerjan@gmail.com>
2023-09-26 16:50:01 +0200

    [add] styling

--- src/components/molecules/CountryCard/CountryCard.tsx
+++ src/components/molecules/CountryCard/CountryCard.tsx
@@ -1,8 +1,9 @@
 import { Country } from "../../../types/country.type";
+import './CountryCard.css';
 
 function CountryCard({country}: {country: Country}){
     return (
-        <div>
+        <div className="card">
             <h1>{country.name.official}</h1>
             <p>{country.capital}</p>
             <p>{country.region}</p>
