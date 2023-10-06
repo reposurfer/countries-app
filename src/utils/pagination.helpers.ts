ce804778ce8c52b13a3be46d451d2e0fdb614510
Stef Boerjan <stef.boerjan@gmail.com>
2023-10-06 15:18:17 +0200

    [chore] added types to pagination helpers

--- src/utils/pagination.helpers.ts
+++ src/utils/pagination.helpers.ts
@@ -1,10 +1,10 @@
-export function scrollToTopAnimated(duration: number) {
-    const start = window.scrollY;
-    const startTime = performance.now();
+export function scrollToTopAnimated(duration: number): void {
+    const start: number = window.scrollY;
+    const startTime: number = performance.now();
   
-    function scrollStep(timestamp: number) {
-      const currentTime = timestamp || performance.now();
-      const elapsedTime = currentTime - startTime;
+    function scrollStep(timestamp: number): void {
+      const currentTime: number = timestamp || performance.now();
+      const elapsedTime: number = currentTime - startTime;
   
       window.scrollTo(0, easeInOutCubic(elapsedTime, start, -start, duration));
   
@@ -14,7 +14,7 @@ export function scrollToTopAnimated(duration: number) {
     }
   
     // Easing function for smoother animation (you can adjust this if needed)
-    function easeInOutCubic(t: number, b: number, c: number, d: number) {
+    function easeInOutCubic(t: number, b: number, c: number, d: number): number {
       t /= d / 2;
       if (t < 1) return (c / 2) * t * t * t + b;
       t -= 2;
